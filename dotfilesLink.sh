3fb0da83abc184b0f3b60e13dce7b0e6e3761a48
Kengo Obana <aoyama.banaoh@gmail.com>
2017-04-27 14:17:59 +0900

    add .vim/buckup

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,4 +1,11 @@
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/colors ~/.vim
+# ln -sf ~/dotfiles/.vimrc ~/.vimrc
+# ln -sf ~/dotfiles/.zshrc ~/.zshrc
+# ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+# ln -sf ~/dotfiles/colors ~/.vim
+
+T_FILES=(.vimrc .zshrc .tmux.conf .vim)
+for file in ${DOT_FILES[@]}
+do
+    ln -s $HOME/dotfiles/$file $HOME/$file
+done
+
