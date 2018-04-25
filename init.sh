6f31675dec71a0a1a691d04f5c2f06e0bcacb5be
Kengo Obana <aoyama.banaoh@gmail.com>
2018-04-26 02:05:46 +0900

    インストール箇所を加筆

--- init.sh
+++ init.sh
@@ -3,37 +3,58 @@
 brew --version
 if [ $? -eq 0 ]
 then
-  echo 入ってる
+  echo already installed homebrew
 else
-  echo 入ってない
-  #brew install
+  echo brew install homebrew
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  brew update
+  echo brew installed!!
 fi
 
 # zshの確認
 brew list zsh
 if [ $? -eq 0 ]
 then
-  echo 入ってる
+  echo already installed zsh
 else
-  echo 入ってない
+  echo brew install zsh
+  brew install --without-etcdir zsh
+
+  echo ログインシェルをzshにします
+  echo -n "${MSG} [Y/N or y/n]: "
+  read ans 
+  case $ans in
+    [yY]) 
+      cat /usr/local/bin/zsh >> /etc/shells
+      chsh -s /usr/local/bin/zsh
+      echo $SHELL
+      echo zshであればok
+
+      echo oh-my-zshをインストール
+      sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    [nN]) return 1 ;;
+  esac
 fi
 
 # tmuxの確認
 brew list tmux
 if [ $? -eq 0 ]
 then
-  echo 入ってる
+  echo already installed tmux
 else
-  echo 入ってない
+  echo brew install tmux
+  brew install tmux
 fi
 
 # git clone
 brew list git
 if [ $? -eq 0 ]
 then
-  echo 入ってる
+  echo already installed git
 else
-  echo 入ってない
+  echo brew install git
+  brew install git
+  echoo ins
 fi
 
 # symbolic link
